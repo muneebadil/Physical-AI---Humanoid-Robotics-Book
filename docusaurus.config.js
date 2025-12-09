028ab6cbf2fe43cab4822cac51086287aea6d5e4
muneebadil <mohammadmuneeb197@gmail.com>
2025-12-09 18:33:20 +0500

    Updated docusaurus config & fixed URLs for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -171,7 +171,7 @@ const config = {
     v4: true,
   },
 
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://physical-ai-humanoid-robotics-book-olive.vercel.app',
   baseUrl: '/',
 
   organizationName: 'muneebadil',
